069dd8d5beab73139af5f911dc63fe9daa9b4f01
ArtemMorozko <113714186+ArtemMorozko@users.noreply.github.com>
2023-10-06 10:00:44 +0200

    Update main.js

--- javascript_1/main.js
+++ javascript_1/main.js
@@ -1,5 +1,6 @@
 var button = document.querySelector("#changeColorText");
 var text = document.querySelector("#textBlack");
 button.onclick = function() {
-    text.classList.toggle("textGreen");
-};
+    text.classList.toggle("textGreen"); 
+    text.style.color = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`
+});
