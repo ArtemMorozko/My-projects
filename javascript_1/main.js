21c0689f5d2e79cb52ab93f5fd05e8004c5a91a7
ArtemMorozko <113714186+ArtemMorozko@users.noreply.github.com>
2023-10-06 09:53:42 +0200

    Update main.js

--- javascript_1/main.js
+++ javascript_1/main.js
@@ -1,5 +1,5 @@
 var button = document.querySelector("#changeColorText");
 var text = document.querySelector("#textBlack");
 button.onclick = function() {
-    text.classList.add("textGreen");
+    text.classList.toggle("textGreen");
 };
